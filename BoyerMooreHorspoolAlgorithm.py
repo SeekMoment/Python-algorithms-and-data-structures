892204ac854031d49acfa3fa7c2821078b871286
Matveyev Maxim <bewundernswertmm@gmail.com>
2021-10-28 14:51:13 +0700

    Added Dijkstra Algorithm and fixed an error in Boyer-Moore-Horspool Algorithm

--- BoyerMooreHorspoolAlgorithm.py
+++ BoyerMooreHorspoolAlgorithm.py
@@ -1,42 +1,52 @@
-t = 'data'
+t = "данные"
 
-S = set()
-M = len(t)
-d = {}
-for i in range(M-2, -1, -1):
-    if t[i] not in S:
+# Этап 1: формирование таблицы смещений
+
+S = set()  # уникальные символы в образе
+M = len(t)  # число символов в образе
+d = {}  # словарь смещений
+
+for i in range(M - 2, -1, -1):  # итерации с предпоследнего символа
+    if t[i] not in S:  # если символ еще не добавлен в таблицу
         d[t[i]] = M - i - 1
         S.add(t[i])
 
-if t[M - 1] not in S:
+if t[M - 1] not in S:  # отдельно формируем последний символ
     d[t[M - 1]] = M
 
-d['*'] = M
+d['*'] = M  # смещения для прочих символов
 
-a = 'big weather dta'
+print(d)
+
+# Этап 2: поиск образа в строке
+
+a = "метеоданные"
 N = len(a)
 
 if N >= M:
-    i = M - 1
-    j = 0
-    while i < N:
+    i = M - 1  # счетчик проверяемого символа в строке
+
+    while (i < N):
         k = 0
+        j = 0
+        flBreak = False
         for j in range(M - 1, -1, -1):
             if a[i - k] != t[j]:
                 if j == M - 1:
-                    off = d[a[i]] if d.get(a[i], False) else d['*']
+                    off = d[a[i]] if d.get(a[i], False) else d['*']  # смещение, если не равен последний символ образа
                 else:
-                    off = d[t[j]]
+                    off = d[t[j]]  # смещение, если не равен не последний символ образа
 
-                i += off
+                i += off  # смещение счетчика строки
+                flBreak = True  # если несовпадение символа, то flBreak = True
                 break
 
-            k += 1
+            k += 1  # смещение для сравниваемого символа в строке
 
-        if j == 0:
-            print(f'Pattern found by index {i - k + 1}')
+        if not flBreak:  # если дошли до начала образа, значит, все его символы совпали
+            print(f"образ найден по индексу {i - k + 1}")
             break
     else:
-        print('Pattern not found')
+        print("образ не найден")
 else:
-    print('Pattern not found')
+    print("образ не найден")
